e2b383faca9efe93782a3b26f4742a0f48bfd740
Tawfeeq Zaghlool <tawfeeq.zaghlool@gmail.com>
2022-10-13 00:27:11 +0300

    Packages Updated

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -81,38 +81,7 @@ const Navigation = () => {
 								className="align-items-lg-center ml-lg-auto"
 								navbar
 							>
-								{socialLinks.facebook && (
-									<NavItem>
-										<NavLink
-											rel="noopener"
-											aria-label="Facebook"
-											className="nav-link-icon"
-											href={socialLinks.facebook}
-											target="_blank"
-										>
-											<i className="fa fa-facebook-square" />
-											<span className="nav-link-inner--text d-lg-none ml-2">
-												Facebook
-											</span>
-										</NavLink>
-									</NavItem>
-								)}
-								{socialLinks.instagram && (
-									<NavItem>
-										<NavLink
-											rel="noopener"
-											aria-label="Instagram"
-											className="nav-link-icon"
-											href={socialLinks.instagram}
-											target="_blank"
-										>
-											<i className="fa fa-instagram" />
-											<span className="nav-link-inner--text d-lg-none ml-2">
-												Instagram
-											</span>
-										</NavLink>
-									</NavItem>
-								)}
+								
 								{socialLinks.github && (
 									<NavItem>
 										<NavLink
@@ -141,22 +110,6 @@ const Navigation = () => {
 											<i className="fa fa-linkedin" />
 											<span className="nav-link-inner--text d-lg-none ml-2">
 												Linkedin
-											</span>
-										</NavLink>
-									</NavItem>
-								)}
-								{socialLinks.twitter && (
-									<NavItem>
-										<NavLink
-											rel="noopener"
-											aria-label="Twitter"
-											className="nav-link-icon"
-											href={socialLinks.twitter}
-											target="_blank"
-										>
-											<i className="fa fa-twitter-square" />
-											<span className="nav-link-inner--text d-lg-none ml-2">
-												Twitter
 											</span>
 										</NavLink>
 									</NavItem>
